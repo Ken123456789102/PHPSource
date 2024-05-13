ea0da9b9f148436569ed4be8acbecaa646d94020
Sanou(mnph) <128389192+Ken123456789102@users.noreply.github.com>
2024-05-13 21:35:36 +0700

    Add files via upload

---
+++ Week3/BMProject/MVC/Bridge.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<?php 
+require_once "../MVC/processing/Application.php";
+require_once "../MVC/processing/Controller.php";
+require_once "../MVC/processing/DBManager.php";   #ten lop phai trung ten file (phan biet hoa thuong)
+?>
